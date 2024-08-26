d16429b834d6cb6ad7787f79c687187f5035f198
sespect101 <z2zd@hotmail.com>
2024-08-26 10:35:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const axios = require('axios');
 const fs = require('fs').promises;
 const path = require('path');
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
